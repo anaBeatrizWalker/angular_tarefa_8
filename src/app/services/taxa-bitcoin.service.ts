260af49a4e12803be39e805bcc3ed342322cd23d
anaBeatrizWalker <95242519+anaBeatrizWalker@users.noreply.github.com>
2022-11-11 21:25:43 -0300

    Adicionando service e HttpClient

--- src/app/services/taxa-bitcoin.service.ts
+++ src/app/services/taxa-bitcoin.service.ts
@@ -1,8 +1,40 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+interface Response {
+  time: {
+    updated: string;
+  };
+  bpi: {
+    [key in 'USD' | 'BRL']: {
+      rate_float: number;
+    };
+  };
+}
+
+interface RatesList {
+  timestamp: string;
+  USD: number;
+  BRL: number;
+}
 
 @Injectable()
 export class TaxaBitcoinService {
+  currentRates: Response;
+  ratesList: Array<RatesList> = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
+  getRates() {
+    this.http
+      .get<Response>('https://api.coindesk.com/v1/bpi/currentprice/BRL.json')
+      .subscribe((data) => {
+        this.currentRates = data;
+        this.ratesList.push({
+          timestamp: this.currentRates.time.updated,
+          USD: this.currentRates.bpi.USD.rate_float,
+          BRL: this.currentRates.bpi.BRL.rate_float,
+        });
+      });
+  }
 }
--- src/app/taxa-bitcoin/taxa-bitcoin.component.ts
+++ src/app/taxa-bitcoin/taxa-bitcoin.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { TaxaBitcoinService } from '../services/taxa-bitcoin.service';
 
 @Component({
   selector: 'app-taxa-bitcoin',
   templateUrl: './taxa-bitcoin.component.html',
-  styleUrls: ['./taxa-bitcoin.component.css']
+  styleUrls: ['./taxa-bitcoin.component.css'],
 })
 export class TaxaBitcoinComponent implements OnInit {
-
-  constructor() { }
+  constructor(public taxaBitcoinService: TaxaBitcoinService) {}
 
   ngOnInit() {
+    this.taxaBitcoinService.getRates();
   }
 
+  getCurrentRate() {
+    return this.taxaBitcoinService.currentRates;
+  }
+
+  update() {
+    this.taxaBitcoinService.getRates();
+  }
 }
